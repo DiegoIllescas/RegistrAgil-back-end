ea491cb787d2e2e5595f3a4b95b59192dc381afe
Diego Illescas <146777739+DiegoIllescas@users.noreply.github.com>
2024-05-20 13:36:43 -0600

    Base de las peticiones con token Auth

--- utils.php
+++ utils.php
@@ -1,4 +1,9 @@
 <?php
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
+
+    require 'vendor/autoload.php';
+
     function connect($db) {
         try {
             $dsn = "mysql:host={$db['host']};dbname={$db['db']};charset=UTF8;port={$db['port']}";
@@ -10,8 +15,31 @@
             $conn = new PDO($dsn, $db['user'], $db['pass'], $options);
             return $conn;
         } catch (PDOException $e) {
-            echo $e->getMessage();
             return null;
+        }
+    }
+
+    function newPassword() {
+        $bytes = openssl_random_pseudo_bytes(8);
+        return bin2hex($bytes);
+    }
+
+    function getToken($header) {
+        $auth = explode(' ', $header);
+        return $auth[1];
+    } 
+
+    function isAuth($headers) {
+        if (array_key_exists('Authorization', $headers)) {
+            $token = getToken($headers['Authorization']);
+            try {
+                $decodeToken = (array) JWT::decode($token, new Key('PASSWORD_TEST', 'HS256'));
+                return 200;
+            } catch (\Throwable $th) {
+                return 308;
+            }
+        } else {
+            return 401;
         }
     }
 ?>
