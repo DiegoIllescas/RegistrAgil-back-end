5b8f90aa5f8cf6eb5cffb9fd993ad392e258baa2
Diego Illescas <dillescasl1800@alumno.ipn.mx>
2024-05-24 20:07:22 -0600

    base usuario

---
+++ usuario.php
@@ -0,0 +1,36 @@
+<?php
+    include "config.php";
+    include "utils.php";
+    header("Content-Type: application/json; charset=utf-8");
+
+    //Lectura de los headers de la peticion
+    $headers = apache_request_headers();
+    $isAuth = isAuth($headers, $keypass);
+    
+    //Error si el Token de Sesion expiro
+    if($isAuth['status'] == 432) {
+        header("HTTP/1.1 308 Session Expired");
+        echo json_encode(['success' => false, 'error' => 'Sesion expirada']);
+        exit();
+    }
+
+    //Error si no incluye el Token de Autenticacion
+    if($isAuth['status'] == 401) {
+        header("HTTP/1.1 401 Unauthorized");
+        echo json_encode(['success' => false, 'error' => 'No estas logueado']);
+        exit();
+    }
+    
+    $userData = $isAuth['payload'];
+
+    $dbConn = connect($db);
+
+    //Si no se pudo conectar a la base
+    if(!$dbConn) {
+        header("HTTP/1.1 503 Service Unavailable");
+        echo json_encode(['success' => false, 'error' => 'Servicio no disponible']);
+        exit();
+    }
+
+
+?>
